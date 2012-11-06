27e4997320209c3c0f223f5ee49925a3099cfc3f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-05 19:00:22 -0700

    Updated

--- source/prep/collections/Where.cs
+++ source/prep/collections/Where.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using prep.utility.filtering;
 
 namespace prep.collections
 {
+  public delegate ProductionStudio AMoviesProductionStudio(Movie movie);
+
   public class Where<ItemToFind>
   {
-    public static void has_a()
+    public static AMoviesProductionStudio has_a(AMoviesProductionStudio accessor)
     {
-      throw new NotImplementedException();
+      return accessor;
+    }
+  }
+
+  public static class MovieExtensions
+  {
+    public static IMatchAn<Movie> equal_to(this AMoviesProductionStudio accessor,
+                                           ProductionStudio value)
+    {
+      return new AnonymousCondition<Movie>(x => x.production_studio == value);
     }
   }
 }
