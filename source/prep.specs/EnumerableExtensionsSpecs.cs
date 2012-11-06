cbd843eb30f93312d2450ba76237dc8654af9b78
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-06 15:06:07 -0700

    Updated

---
+++ source/prep.specs/EnumerableExtensionsSpecs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using prep.utility;
+using prep.utility.filtering;
+
+namespace prep.specs
+{
+  [Subject(typeof(EnumerableExtensions))]
+  public class EnumerableExtensionsSpecs
+  {
+    public abstract class concern : Observes
+    {
+    }
+
+    public class when_filtering_a_set_of_items_by_a_condition : concern
+    {
+      Establish c = () =>
+      {
+        numbers = Enumerable.Range(1, 7);
+      };
+
+      Because b = () =>
+        results = numbers.all_items_matching(new AnonymousCondition<int>(x => x%2 == 0));
+
+      It should_only_return_the_items_that_meet_the_condition = () =>
+        results.ShouldContain(2, 4, 6);
+
+      static IEnumerable<int> results;
+      static IEnumerable<int> numbers;
+    }
+  }
+}
---
+++ source/prep/utility/filtering/EnumerableFilteringExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace prep.utility.filtering
+{
+  public static class EnumerableFilteringExtensions
+  {
+    public static FilteringEnumerable<ItemToFilter, PropertyType> where<ItemToFilter, PropertyType>(
+      this IEnumerable<ItemToFilter> items,
+      PropertyAccessor
+        <ItemToFilter, PropertyType> accessor)
+    {
+      return new FilteringEnumerable<ItemToFilter, PropertyType>(items, accessor);
+    }
+  }
+
+  public class FilteringEnumerable<ItemToFilter, PropertyType> : IEnumerable<ItemToFilter>
+  {
+    IEnumerable<ItemToFilter> original_set;
+    PropertyAccessor<ItemToFilter, PropertyType> accessor;
+
+    public FilteringEnumerable(IEnumerable<ItemToFilter> original_set,
+                               PropertyAccessor<ItemToFilter, PropertyType> accessor)
+    {
+      this.original_set = original_set;
+      this.accessor = accessor;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    public IEnumerator<ItemToFilter> GetEnumerator()
+    {
+      return original_set.GetEnumerator();
+    }
+
+    public IEnumerable<ItemToFilter> equal_to_any(params PropertyType[] values)
+    {
+      return original_set.all_items_matching(Where<ItemToFilter>.has_a(accessor).equal_to_any(values));
+    }
+  }
+}
