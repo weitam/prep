85022e0a9bcd02bf21737eb7e7ab415be027f742
Roger Sillito <rogersillito@gmail.com>
2012-11-06 19:03:19 -0700

    Pushing new changes

---
+++ source/prep/utility/filtering/Order.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace prep.utility.filtering
+{
+    //public delegate
+
+    public class Order<TTypeToSort>
+    {
+         public static IComparer<TTypeToSort> by_descending(PropertyAccessor<TTypeToSort, > property)
+         {
+             return new DescendingComparer<TTypeToSort>();
+         }
+    }
+
+
+
+
+    public class DescendingComparer<TTypeToSort> : IComparer<TTypeToSort>
+    {
+        public int Compare(TTypeToSort x, TTypeToSort y)
+        {
+            return x
+        }
+    }
+}
