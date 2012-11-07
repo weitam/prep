8a2e849d8f55ddb039e00be9ded8f4aaa5be1337
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-06 19:44:56 -0700

    Updated

---
+++ source/prep/utility/filtering/ComparerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prep.utility.filtering
+{
+  public static class ComparerExtensions
+  {
+    public static IComparer<TItemToSort> then_by<TItemToSort, TPropertyTypeToSortOn>(this IComparer<TItemToSort> comparer,PropertyAccessor<TItemToSort,TPropertyTypeToSortOn> accessor)
+    {
+      throw new NotImplementedException(); 
+    }
+  }
+}
